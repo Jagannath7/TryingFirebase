31307d8ecc4042437840a9b3f024c9c564491f47
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-08-28 23:24:04 +0530

    read data from firebase

--- app/src/main/java/com/example/tryingfirebase/MainActivity.kt
+++ app/src/main/java/com/example/tryingfirebase/MainActivity.kt
@@ -2,25 +2,58 @@ package com.example.tryingfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val dbRef = FirebaseDatabase.getInstance().reference
+    val list = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtBtn.setOnClickListener {
+        val adapter = ArrayAdapter<String>(
+            this, R.layout.item_row,
+            R.id.viewList,
+            list
+        )
+
+        listView.adapter = adapter
+
+        Btn.setOnClickListener {
             val text = edtText.text.toString()
-            dbRef.child("text").push().setValue(text)
+            dbRef.child("Text").push().setValue(text)
         }
 
-        numBtn.setOnClickListener {
-            val num = edtText.text.toString()
-            dbRef.child("number").push().setValue(num)
-        }
+       dbRef.child("Text").addChildEventListener(object : ChildEventListener {
+
+           override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+               val text = snapshot.getValue()
+               text.let{list.add(it as String)}
+               adapter.notifyDataSetChanged()
+           }
+
+           override fun onCancelled(error: DatabaseError) {
+
+           }
+
+           override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+
+           }
+
+           override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+
+           }
+
+           override fun onChildRemoved(snapshot: DataSnapshot) {
+
+           }
+       })
     }
 }
