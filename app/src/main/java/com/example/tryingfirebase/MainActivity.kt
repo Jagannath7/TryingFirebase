2f38d8b2926e894c54313ca82d1dafa3135a63e6
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-08-28 22:00:57 +0530

    write data into firebase

--- app/src/main/java/com/example/tryingfirebase/MainActivity.kt
+++ app/src/main/java/com/example/tryingfirebase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.tryingfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val dbRef = FirebaseDatabase.getInstance().reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txtBtn.setOnClickListener {
+            val text = edtText.text.toString()
+            dbRef.child("text").push().setValue(text)
+        }
+
+        numBtn.setOnClickListener {
+            val num = edtText.text.toString()
+            dbRef.child("number").push().setValue(num)
+        }
     }
 }
